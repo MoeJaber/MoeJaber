4ba56cca8d096c9038c666688ae1a93980e5f98b
MoeJaber <mouhamadjaber@live.com>
2017-10-18 15:30:05 -0400

    First Commit

---
+++ Assignment_3_Jaber_Mouhamad/Assignment_3_Jaber_Mouhamad/main.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Assignment_3_Jaber_Mouhamad
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.map_tab = new MetroFramework.Controls.MetroTabPage();
+            this.data_tab = new MetroFramework.Controls.MetroTabPage();
+            this.main_DataFromCSVBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.metroTabControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.main_DataFromCSVBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.map_tab);
+            this.metroTabControl1.Controls.Add(this.data_tab);
+            this.metroTabControl1.Location = new System.Drawing.Point(23, 54);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(700, 336);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // map_tab
+            // 
+            this.map_tab.HorizontalScrollbarBarColor = true;
+            this.map_tab.HorizontalScrollbarHighlightOnWheel = false;
+            this.map_tab.HorizontalScrollbarSize = 10;
+            this.map_tab.Location = new System.Drawing.Point(4, 38);
+            this.map_tab.Name = "map_tab";
+            this.map_tab.Size = new System.Drawing.Size(692, 294);
+            this.map_tab.TabIndex = 2;
+            this.map_tab.Text = "Map";
+            this.map_tab.VerticalScrollbarBarColor = true;
+            this.map_tab.VerticalScrollbarHighlightOnWheel = false;
+            this.map_tab.VerticalScrollbarSize = 10;
+            // 
+            // data_tab
+            // 
+            this.data_tab.AutoScroll = true;
+            this.data_tab.HorizontalScrollbar = true;
+            this.data_tab.HorizontalScrollbarBarColor = true;
+            this.data_tab.HorizontalScrollbarHighlightOnWheel = false;
+            this.data_tab.HorizontalScrollbarSize = 10;
+            this.data_tab.Location = new System.Drawing.Point(4, 38);
+            this.data_tab.Name = "data_tab";
+            this.data_tab.Size = new System.Drawing.Size(692, 294);
+            this.data_tab.TabIndex = 3;
+            this.data_tab.Text = "Data";
+            this.data_tab.VerticalScrollbar = true;
+            this.data_tab.VerticalScrollbarBarColor = true;
+            this.data_tab.VerticalScrollbarHighlightOnWheel = false;
+            this.data_tab.VerticalScrollbarSize = 10;
+            // 
+            // main_DataFromCSVBindingSource
+            // 
+            this.main_DataFromCSVBindingSource.DataSource = typeof(Assignment_3_Jaber_Mouhamad.main.DataFromCSV);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 413);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "main";
+            this.Text = "Main";
+            this.metroTabControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.main_DataFromCSVBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage map_tab;
+        private MetroFramework.Controls.MetroTabPage data_tab;
+        private System.Windows.Forms.BindingSource main_DataFromCSVBindingSource;
+    }
+}
+
---
+++ Assignment_3_Jaber_Mouhamad/Assignment_3_Jaber_Mouhamad/main.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using MetroFramework.Forms;
+using LiveCharts.Defaults;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Collections.ObjectModel;
+
+//http://open.canada.ca/en/open-government-licence-canada 
+
+namespace Assignment_3_Jaber_Mouhamad
+{
+    public partial class main : MetroForm
+    {
+        private static ObservableCollection<DataFromCSV> rowResults = new ObservableCollection<DataFromCSV>();
+      
+
+        public main()
+        {
+            InitializeComponent();
+            ReadCSV();
+        
+            foreach(var s in rowResults)
+            {
+                Console.WriteLine("---------\n" + s.Ref_Date + "\n" + s.GEO + "\n" + s.EST + "\n" + s.Vector + "\n" + s.Coordinate + "\n" + s.Value + "\n" + s.Geography + "\n" + s.Area + "\n-------- ");
+            }
+
+           
+        }//end of main
+
+        public void ReadCSV()
+        {
+            int numOfColumns = 6;
+            string loc = "";
+            string production = "";
+
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Jaber.M.GDC\Downloads\00010014-eng\00010014-eng.csv"))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+               
+                    //Processing row
+                    string[] col = parser.ReadFields();
+
+                    for (var x = 0; x < numOfColumns; x++)
+                    {
+                        ColNames.Columns.Add(col[x]);
+                    }
+
+                while (!parser.EndOfData)
+                {
+                   
+                    string[] fields = parser.ReadFields();
+
+                    string [] array = fields[4].Split(new char[] { '.' }).Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                    var geography = Convert.ToInt16(array[0]);
+                    var area = Convert.ToInt16(array[1]);                  
+
+                    switch (geography)
+                    #region                   
+                    {                
+                    case 1: //Canada
+                            loc = "Canada";
+                            break;
+                        case 2: //Newfoundland and Labrador
+                            loc = "Newfoundland and Labrador";
+                            break;
+                        case 3: //Prince Edward Island
+                            loc = "Prince Edward Island";
+                            break;
+                        case 4: //Nova Scotia
+                            loc = "Nova Scotia";
+                            break;
+                        case 5: //New Brunswick
+                            loc = "New Brunswick";
+                            break;
+                        case 6: //Quebec
+                            loc = "Quebec";
+                            break;
+                        case 7: //Ontario
+                            loc = "Ontario";
+                            break;
+                        case 8: //Manitoba
+                            loc = "Manitoba";
+                            break;
+                        case 9: //Saskatchewan
+                            loc = "Saskatchewan";
+                            break;
+                        case 10: //Alberta
+                            loc = "Alberta";
+                            break;
+                        case 11: //British Columbia
+                            loc = "British Columbia";
+                        break;
+#endregion
+                    }
+
+                    switch (area)
+                    #region                   
+                    {
+                        case 1: //Seeded area, potatoes (acres) 
+                            production = "Seeded area, potatoes (acres) ";
+                            break;
+                        case 2: //Average yield, potatoes (hundredweight per harvested acres)
+                            production = "Average yield, potatoes (hundredweight per harvested acres)";
+                            break;
+                        case 3: //Production, potatoes (hundredweight)
+                            production = "Production, potatoes (hundredweight)";
+                            break;
+                        case 4: //Average farm price, potatoes (dollars per hundredweight)                        
+                            production = "Average farm price, potatoes (dollars per hundredweight)";
+                            break;
+                        case 5: //Total farm value, potatoes (dollars)
+                            production = "Total farm value, potatoes (dollars)";
+                            break;
+                        case 6: //Harvested area, potatoes (acres)
+                            production = "Harvested area, potatoes (acres)";
+                            break;
+                        case 7: //Amount sold, consumed, seeded or fed to livestock, potatoes (hundredweight)
+                            production = "Amount sold, consumed, seeded or fed to livestock, potatoes (hundredweight)";
+                            break;                      
+                            #endregion
+                    }
+
+                    rowResults.Add(new DataFromCSV() { Ref_Date = fields[0], GEO = fields[1], EST = fields[2], Vector = fields[3], Coordinate = fields[4], Value = fields[5], Geography = loc, Area = production });
+               }
+            }
+        }//end of ReadCSV
+
+        public class ColNames
+        {
+            public static List<String> Columns = new List<string>();
+        }
+
+        internal class DataFromCSV
+        {
+            public String Ref_Date { get; set; }
+            public String GEO { get; set; }
+            public String EST { get; set; }
+            public String Vector { get; set; }
+            public String Coordinate { get; set; }
+            public String Value { get; set; }
+            public String Geography { get; set; }
+            public String Area { get; set; }
+        }
+    }
+}
